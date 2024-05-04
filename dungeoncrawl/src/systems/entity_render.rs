7f0959d1800c44f40e6e0265ae0c25457fb13697
Malte Friedrich <malte@friedrich-burhave.de>
2024-05-04 22:15:00 +0200

    DungeonCrawler: Adding monster spawning and collisions

--- dungeoncrawl/src/systems/entity_render.rs
+++ dungeoncrawl/src/systems/entity_render.rs
@@ -8,9 +8,11 @@ pub fn entity_render(ecs: &SubWorld, #[resource] camera: &Camera) {
     draw_batch.target(1);
     let offset = Point::new(camera.left_x, camera.top_y);
 
-    <(&Point, &Render)>::query().iter(ecs).for_each(|(pos, render)| {
-        draw_batch.set(*pos - offset, render.color, render.glyph);
-    });
+    <(&Point, &Render)>::query()
+        .iter(ecs)
+        .for_each(|(pos, render)| {
+            draw_batch.set(*pos - offset, render.color, render.glyph);
+        });
 
     draw_batch.submit(5000).expect("Batch error");
 }
